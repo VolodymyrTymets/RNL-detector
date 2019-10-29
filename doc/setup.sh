3e0f729c7ae5056a4f7b798cd04e729487c3baee
volodymyr <volodymyrtymets@gmail.com>
2019-10-29 21:26:38 +0200

    update setup

--- doc/setup.sh
+++ doc/setup.sh
@@ -1,14 +1,15 @@
 # Install programs
 
+# Raspy to router
+sudo apt install network-manager network-manager-gnome openvpn \openvpn-systemd-resolved network-manager-openvpn \network-manager-openvpn-gnome
+sudo apt purge openresolv dhcpcd5
+
 # git istall
-
 sudo apt-get install git-core
-
 sudo apt-get install nodejs
 sudo apt-get install npm
 sudo npm install -g n
 sudo n 10.15.3
-
 
 #Pm2 - to autorun on launch
 sudo npm i -g pm2
@@ -17,14 +18,16 @@ sudo pm2 startup
 #chec if success
 sudo systemctl status pm2
 
-
-#Copy projects rectangle generator
+#Copy project rectangle generator
 cd Documets/
 mkdir apps
 cd apps
 git clone https://github.com/VolodymyrTymets/rectangle-generator.git
 cd rectangle-generator
 npm i
+
+#Copy project sound up
+
 
 #run via pm2
 npm run autorun
